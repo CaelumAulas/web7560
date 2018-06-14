2e6ef78c39ad54feaef144cf3c5ab0f1f7ff340c
MarcoBrunoBR <marco.bruno.br@gmail.com>
2018-06-14 19:30:59 -0300

    Fim da quarta aula

--- animacao/js/card.js
+++ animacao/js/card.js
@@ -1,17 +1,52 @@
-
 ;(function () {
-  "use strict"  
+  "use strict"
 
-  nome = 'Marco'
-  const email = 'marco.bruno.br@gmail.com'
-  const $cardMenuItem = document.querySelector('.card-menu-item')
+  const $card = document.querySelector('.card')
+  const $cardMenuItems = document.querySelectorAll('.card-menu-item')
+  const $section = document.querySelector('#listaCartoes')
 
-  $cardMenuItem.addEventListener('click', () => {
-    const $card = $cardMenuItem.parentNode.parentNode
+  $section.addEventListener('click', function(event) {
+    const $origin = event.target
+    
+    if ($origin.classList.contains('card-menu-item')) {
+      const $card = $origin.parentNode.parentNode
+      removeCartaoDeFormaAnimada($card)
+    }
+  })
 
+  $card.addEventListener('keydown', () => {
+    // console.log('Apertando')
+  })
+
+  $card.addEventListener('keyup', function(event) {
+    const $origin = event.target
+    const keyPress = event.key
+
+    if ($origin.tagName === 'BUTTON' && 
+        $origin.classList.contains('card-menu-item') && 
+        keyPress === 'Delete') {
+      $origin.click()
+    }
+  })
+
+  $card.addEventListener('focusin', function(event) {
+    // console.log('Elemento que disparou o evento:', event.target)
+    this.classList.add('card_focus')
+  })
+
+  $card.addEventListener('focusout', function() {
+    this.classList.remove('card_focus')
+  })
+
+  // $cardMenuItems.forEach(($cardMenuItem)=> {
+  //   $cardMenuItem.addEventListener('click', removeCartaoDeFormaAnimada)
+  // })
+
+  function removeCartaoDeFormaAnimada($card) {
     $card.classList.add('card_remove')
     $card.addEventListener('transitionend', () => {
       $card.remove()
     })
-  })
-})
+  }
+  
+})()
